d4eb7a06de82182397c1da2eaae7db4fcf859c68
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2019-06-03 16:32:48 +0200

    fixed installation command

--- setup/kafka/install-kafka-libs-2v1.sh
+++ setup/kafka/install-kafka-libs-2v1.sh
@@ -5,11 +5,8 @@ mkdir /kafka  > /dev/null 2>&1 || true
 cd /kafka
 git clone https://github.com/edenhill/librdkafka
 cd librdkafka
-git checkout v0.11.1 && \
-./configure
-make
-sudo make install
-sudo ldconfig
+git checkout v0.11.1
+./configure && make && sudo make install && sudo ldconfig
 
 # Installing official python client which is based on librdkafka
 pip3 install confluent-kafka==0.11.6
