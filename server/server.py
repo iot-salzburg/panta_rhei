454bfe774aa1caba922d20a32f2b032bc2d127ac
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2019-08-13 09:05:06 +0200

    logging

--- server/server.py
+++ server/server.py
@@ -1,15 +1,13 @@
+import logging
 import psycopg2
 from sqlalchemy import create_engine
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 prefix = "/0v1"
-#engine = create_engine('postgresql+psycopg2://scott:tiger@localhost/mydatabase')
-
 app = Flask(__name__)
 
 # Set up SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////artists.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://iot4cps:iot4cps@localhost/iot4cps'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -21,14 +19,18 @@ class Artist(db.Model):
     birth_year = db.Column(db.Integer)
     genre = db.Column(db.String)
 
-# Create the table
-db.create_all()
-print("Created tables.")
 
-# @app.route(prefix+'/')
-# def example():
-#    return '{"name":"Bob"}'
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route(prefix+'/')
+def example():
+   return '{"name":"Bob"}'
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+    # Create the table
+    db.create_all()
+    logging.info("Created tables.")
+
+    app.run(debug=True)
