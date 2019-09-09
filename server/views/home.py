526cf759962a2fcd9785a5dcd3d36759dc96939a
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2019-09-09 17:23:31 +0200

    Implemented search functionality

--- server/views/home.py
+++ server/views/home.py
@@ -3,6 +3,8 @@ import sqlalchemy as db
 from flask import current_app as app
 from flask import Blueprint, render_template, flash, redirect, url_for, session, request
 
+from .useful_functions import is_logged_in
+
 home_bp = Blueprint("home", __name__)
 
 
@@ -21,10 +23,6 @@ def dashboard():
     # Get current user_uuid
     user_uuid = session["user_uuid"]
     msg_systems = msg_companies = None
-
-    # # Set url_for module.function (is used in type.delete_type)
-    # session["last_url"] = str(__name__).split(".")[-1] + "." + inspect.currentframe().f_code.co_name
-    # print(session.get("last_url"))
 
     # Fetch companies, for which the current user is admin of
     engine = db.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
@@ -76,3 +74,91 @@ def about():
 @home_bp.route('/home')
 def home():
     return render_template('home.html')
+
+
+@home_bp.route('/search', methods=['GET', 'POST'])
+@is_logged_in
+def search():
+    search_request = request.args.get('request').strip().lower()
+    app.logger.info("Searching for: {}".format(search_request))
+
+    # Get current user_uuid
+    user_uuid = session["user_uuid"]
+    messages = dict()
+    # msg_systems = msg_companies = msg_clients = msg_streamhub = None
+
+    # Fetch companies, for which the current user is admin of
+    engine = db.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+    conn = engine.connect()
+
+    # fetch dedicated companies
+    query = """SELECT company_uuid, com.*, creator.email AS contact_mail
+    FROM companies AS com 
+    INNER JOIN is_admin_of AS aof ON com.uuid=aof.company_uuid 
+    INNER JOIN users as admin ON admin.uuid=aof.user_uuid
+    INNER JOIN users as creator ON creator.uuid=aof.creator_uuid
+    WHERE admin.uuid='{}';""".format(user_uuid)
+    result_proxy = conn.execute(query)
+    companies = [dict(c.items()) for c in result_proxy.fetchall()]
+    # Filter systems by term
+    companies = [item for item in companies if search_request in str(list(item.values())).lower()]
+    # print("Fetched companies: {}".format(companies))
+    if companies == list():
+        messages["companies"] = "No companies found."
+
+    # fetch dedicated systems
+    query = """SELECT sys.uuid AS system_uuid, domain, enterprise, sys.*, agent.email AS contact_mail
+    FROM systems AS sys
+    INNER JOIN companies AS com ON sys.company_uuid=com.uuid
+    INNER JOIN is_agent_of AS agf ON sys.uuid=agf.system_uuid 
+    INNER JOIN users as agent ON agent.uuid=agf.user_uuid
+    WHERE agent.uuid='{}';""".format(user_uuid)
+    result_proxy = conn.execute(query)
+    systems = [dict(c.items()) for c in result_proxy.fetchall()]
+    # Filter systems by term
+    systems = [item for item in systems if search_request in str(list(item.values())).lower()]
+    if len(systems) == 0:
+        messages["systems"] = "No systems found."
+
+    # fetch dedicated clients
+    query = """SELECT sys.uuid AS system_uuid, name, domain, enterprise, workcenter, station, 
+    creator.email AS contact_mail, clients.*
+    FROM clients
+    INNER JOIN users as creator ON creator.uuid=clients.creator_uuid
+    INNER JOIN systems AS sys ON clients.system_uuid=sys.uuid
+    INNER JOIN companies AS com ON sys.company_uuid=com.uuid
+    INNER JOIN is_agent_of AS agf ON sys.uuid=agf.system_uuid 
+    INNER JOIN users as agent ON agent.uuid=agf.user_uuid
+    WHERE agent.uuid='{}';""".format(user_uuid)
+    result_proxy = conn.execute(query)
+    clients = [dict(c.items()) for c in result_proxy.fetchall()]
+    # Filter systems by term
+    clients = [item for item in clients if search_request in str(list(item.values())).lower()]
+    if len(clients) == 0:
+        messages["clients"] = "No clients found."
+
+    # fetch dedicated streams
+    query = """
+    SELECT sys.uuid AS system_uuid, streams.*, creator.email AS contact_mail
+    FROM streams
+    INNER JOIN users as creator ON creator.uuid=streams.creator_uuid
+    INNER JOIN systems AS sys ON streams.system_uuid=sys.uuid
+    INNER JOIN companies AS com ON sys.company_uuid=com.uuid
+    INNER JOIN is_agent_of AS agf ON sys.uuid=agf.system_uuid 
+    INNER JOIN users as agent ON agent.uuid=agf.user_uuid
+    WHERE agent.uuid='{}';""".format(user_uuid)
+    result_proxy = conn.execute(query)
+    streams = [dict(c.items()) for c in result_proxy.fetchall()]
+    # Filter systems by term
+    streams = [item for item in streams if search_request in str(list(item.values())).lower()]
+    if len(streams) == 0:
+        messages["streams"] = "No streams found."
+
+    engine.dispose()
+    found_count = len(companies) + len(systems) + len(clients) + len(streams)
+    if found_count >= 1:
+        flash("Received {} results for search request '{}'".format(found_count, search_request), "info")
+    else:
+        flash("Received no results for search request '{}'".format(found_count, search_request), "danger")
+    return render_template("search.html", companies=companies, systems=systems, clients=clients, streams=streams,
+                           messages=messages, search_request=search_request)
