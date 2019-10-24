8ce1b296a270b52d040c5886dc9ed1e88865201c
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2019-10-24 12:38:13 +0200

    simplified demo app

--- demo_applications/WeatherService/demo_station_1/demo_station_1.py
+++ demo_applications/WeatherService/demo_station_1/demo_station_1.py
@@ -36,11 +36,10 @@ MAPPINGS = os.path.join(dirname, "ds-mappings.json")
 config = {"client_name": "weatherstation_1",
           "system": "is.iceland.iot-iot4cps-wp5.WeatherService",
           "gost_servers": "localhost:8084",
-          "kafka_bootstrap_servers": "localhost:9092",  # kafka bootstrap server is the preferred way to connect
-          "kafka_rest_server": "localhost:8082"}
+          "kafka_bootstrap_servers": "localhost:9092"}
 client = DigitalTwinClient(**config)
 # client.register_existing(mappings_file=MAPPINGS)
-client.register_new(instance_file=INSTANCES)
+client.register(instance_file=INSTANCES)
 
 randomised_temp = SimulateTemperatures(t_factor=100, day_amplitude=5, year_amplitude=-5, average=2.5)
 
