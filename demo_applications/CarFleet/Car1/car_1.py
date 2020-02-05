7e8fba0172efc3b44c42cc9f4d231b502e760f2a
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2020-02-05 08:57:32 +0100

    Multithreading for sender and receiver

--- demo_applications/CarFleet/Car1/car_1.py
+++ demo_applications/CarFleet/Car1/car_1.py
@@ -17,6 +17,7 @@ import sys
 import inspect
 import time
 import pytz
+import threading
 from datetime import datetime
 
 from client.digital_twin_client import DigitalTwinClient
@@ -29,67 +30,93 @@ INSTANCES = os.path.join(dirname, "instances.json")
 SUBSCRIPTIONS = os.path.join(dirname, "subscriptions.json")
 MAPPINGS = os.path.join(dirname, "ds-mappings.json")
 
-# Set the configs, create a new Digital Twin Instance and register file structure
-# This config is generated when registering a client application on the platform
-# Make sure that Kafka and GOST are up and running before starting the platform
-config = {"client_name": "car_1",
-          "system": "cz.icecars.iot-iot4cps-wp5.CarFleet",
-          "gost_servers": "localhost:8082",
-          "kafka_bootstrap_servers": "localhost:9092"}
-client = DigitalTwinClient(**config)
-# client.register_existing(mappings_file=MAPPINGS)
-client.register_new(instance_file=INSTANCES)  # Registering of new instances should be outsourced to the platform
-client.subscribe(subscription_file=SUBSCRIPTIONS)
 
-car = CarSimulator(track_id=1, time_factor=100, speed=30, cautiousness=1,
-                   temp_day_amplitude=4, temp_year_amplitude=-4, temp_average=3, seed=1)
+def produce_metrics(car, client, interval=10):
+    try:
+        while True:
+            # unix epoch and ISO 8601 UTC are both valid
+            timestamp = datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat()
 
-try:
-    while True:
-        # unix epoch and ISO 8601 UTC are both valid
-        timestamp = datetime.utcnow().replace(tzinfo=pytz.UTC).isoformat()
+            # Measure metrics
+            car.update_positions()
+            temperature = car.temp.get_temp()
+            acceleration = car.get_acceleration()
+            latitude = car.get_latitude()
+            longitude = car.get_longitude()
+            attitude = car.get_attitude()
 
-        # Measure metrics
-        car.update_positions()
-        temperature = car.temp.get_temp()
-        acceleration = car.get_acceleration()
-        latitude = car.get_latitude()
-        longitude = car.get_longitude()
-        attitude = car.get_attitude()
+            # Print the temperature with the corresponding timestamp in ISO format
+            print("The demo car 1 is at [{}, {}],   \twith the temp.: {} °C  \tand had a maximal acceleration of "
+                  "{} m/s²  \tat {}".format(latitude, longitude, temperature, acceleration, timestamp))
 
-        # Print the temperature with the corresponding timestamp in ISO format
-        print("The demo car 1 is at [{}, {}],   \twith the temp.: {} °C  \tand had a maximal acceleration of {} m/s²  "
-              "\tat {}".format(latitude, longitude, temperature, acceleration, timestamp))
+            # Send the metrics via the client, it is suggested to use the same timestamp for later analytics
+            client.produce(quantity="temperature", result=temperature, timestamp=timestamp)
+            client.produce(quantity="acceleration", result=acceleration, timestamp=timestamp)
+            client.produce(quantity="GPS-position-latitude", result=latitude, timestamp=timestamp)
+            client.produce(quantity="GPS-position-longitude", result=longitude, timestamp=timestamp)
+            client.produce(quantity="GPS-position-attitude", result=attitude, timestamp=timestamp)
 
-        # Send the metrics via the client, it is suggested to use the same timestamp for later analytics
-        client.produce(quantity="temperature", result=temperature, timestamp=timestamp)
-        client.produce(quantity="acceleration", result=acceleration, timestamp=timestamp)
-        client.produce(quantity="GPS-position-latitude", result=latitude, timestamp=timestamp)
-        client.produce(quantity="GPS-position-longitude", result=longitude, timestamp=timestamp)
-        client.produce(quantity="GPS-position-attitude", result=attitude, timestamp=timestamp)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        client.disconnect()
 
-        # Receive all queued messages of the weather-service and other connected cars and calculate the minimum
-        minimal_temps = list()
-        if temperature <= 0:
-            minimal_temps.append({"origin": config["system"], "temperature": temperature})
 
-        received_quantities = client.consume(timeout=0.5)
-        for received_quantity in received_quantities:
-            # The resolves the all meta-data for an received data-point
-            print("  -> Received new external data-point at {}: '{}' = {} {}."
-                  .format(received_quantity["phenomenonTime"],
-                          received_quantity["Datastream"]["name"],
-                          received_quantity["result"],
-                          received_quantity["Datastream"]["unitOfMeasurement"]["symbol"]))
-            # To view the whole data-point in a pretty format, uncomment:
-            # print("Received new data: {}".format(json.dumps(received_quantity, indent=2)))
-            if received_quantity["result"] <= 0:
-                minimal_temps.append(
-                    {"origin": received_quantity["Datastream"]["name"], "temperature": received_quantity["result"]})
+# Receive all temperatures of the weather-service and other cars and check whether they are subzero
+def consume_metrics(client):
+    try:
+        while True:
+            # In this list, each datapoint is stored that yiels a result below zero degC.
+            subzero_temp = list()
 
-        if minimal_temps != list():
-            print("    WARNING, the road could be slippery, see: {}".format(minimal_temps))
+            # Data of the same instance can be consumed directly via the class method
+            temperature = car.temp.get_temp()
+            if temperature < 0:
+                subzero_temp.append({"origin": config["system"], "temperature": temperature})
 
-        time.sleep(5)
-except KeyboardInterrupt:
-    client.disconnect()
+            # Data of other instances (and also the same one) can be consumed via the client
+            received_quantities = client.consume(timeout=1.0)
+            for received_quantity in received_quantities:
+                # The resolves the all meta-data for an received data-point
+                print("  -> Received new external data-point at {}: '{}' = {} {}."
+                      .format(received_quantity["phenomenonTime"],
+                              received_quantity["Datastream"]["name"],
+                              received_quantity["result"],
+                              received_quantity["Datastream"]["unitOfMeasurement"]["symbol"]))
+                # To view the whole data-point in a pretty format, uncomment:
+                # print("Received new data: {}".format(json.dumps(received_quantity, indent=2)))
+                if received_quantity["Datastream"]["unitOfMeasurement"]["symbol"] == "degC" \
+                        and received_quantity["result"] < 0:
+                    subzero_temp.append(
+                        {"origin": received_quantity["Datastream"]["name"], "temperature": received_quantity["result"]})
+
+            # Check whether there are temperatures are subzero
+            if subzero_temp != list():
+                print("    WARNING, the road could be slippery, see: {}".format(subzero_temp))
+
+    except KeyboardInterrupt:
+        client.disconnect()
+
+
+if __name__ == "__main__":
+    # Set the configs, create a new Digital Twin Instance and register file structure
+    # This config is generated when registering a client application on the platform
+    # Make sure that Kafka and GOST are up and running before starting the platform
+    config = {"client_name": "car_1",
+              "system": "cz.icecars.iot-iot4cps-wp5.CarFleet",
+              "gost_servers": "localhost:8082",
+              "kafka_bootstrap_servers": "localhost:9092"}
+    client = DigitalTwinClient(**config)
+    client.register(instance_file=INSTANCES)  # Registering of new instances should be outsourced to the platform
+    client.subscribe(subscription_file=SUBSCRIPTIONS)
+
+    # Create an instance of the CarSimulator that simulates a car driving on different tracks through Salzburg
+    car = CarSimulator(track_id=1, time_factor=100, speed=30, cautiousness=1,
+                       temp_day_amplitude=4, temp_year_amplitude=-4, temp_average=3, seed=1)
+
+    # Create and start the receiver Thread that consumes data via the client
+    receiver = threading.Thread(target=consume_metrics, kwargs=({"client": client}))
+    receiver.start()
+
+    # Create and start the receiver Thread that publishes data via the client
+    sender = threading.Thread(target=produce_metrics, kwargs=({"car": car, "client": client, "interval": 1}))
+    sender.start()
