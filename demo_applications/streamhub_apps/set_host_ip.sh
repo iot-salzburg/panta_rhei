19f9b5e35c17f4d0a36e0435d1717119ed626f38
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2020-02-13 16:00:06 +0100

    Fetching SensorThings ds and filter on metric name and result

--- demo_applications/streamhub_apps/set_host_ip.sh
+++ demo_applications/streamhub_apps/set_host_ip.sh
@@ -4,5 +4,6 @@ export STREAM_NAME="test-stream"
 export SOURCE_SYSTEM=is.iceland.iot4cps-wp5-WeatherService.Stations
 export TARGET_SYSTEM=cz.icecars.iot4cps-wp5-CarFleet.Car1
 export KAFKA_BOOTSTRAP_SERVERS=${HOST_IP}:9092
+export GOST_SERVER=${HOST_IP}:8082
 export FILTER_LOGIC="SELECT 'Station_1.Air Temperature' FROM is.iceland.iot4cps-wp5-WeatherService.Stations
        WHERE 'Station_1.Air Temperature' < 4;"
--- demo_applications/streamhub_apps/src/main/java/com/github/christophschranz/iot4cpshub/StreamAppEngine.java
+++ demo_applications/streamhub_apps/src/main/java/com/github/christophschranz/iot4cpshub/StreamAppEngine.java
@@ -1,5 +1,8 @@
 package com.github.christophschranz.iot4cpshub;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -10,8 +13,15 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyException;
 import java.util.Properties;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 
 /* The StreamAppEngine generates streams between Panta Rhei Systems in Kafka, based on System variables */
 public class StreamAppEngine {
@@ -23,15 +33,17 @@ public class StreamAppEngine {
         logger.info("Starting a new stream with the parameter:");
 
         if (System.getenv().containsKey("STREAM_NAME"))
-            globalOptions.setProperty("stream-name", System.getenv("STREAM_NAME"));
+            globalOptions.setProperty("STREAM_NAME", System.getenv("STREAM_NAME"));
         if (System.getenv().containsKey("SOURCE_SYSTEM"))
-            globalOptions.setProperty("source-system", System.getenv("SOURCE_SYSTEM"));
+            globalOptions.setProperty("SOURCE_SYSTEM", System.getenv("SOURCE_SYSTEM"));
         if (System.getenv().containsKey("TARGET_SYSTEM"))
-            globalOptions.setProperty("target-system", System.getenv("TARGET_SYSTEM"));
+            globalOptions.setProperty("TARGET_SYSTEM", System.getenv("TARGET_SYSTEM"));
         if (System.getenv().containsKey("KAFKA_BOOTSTRAP_SERVERS"))
-            globalOptions.setProperty("bootstrap-server", System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+            globalOptions.setProperty("KAFKA_BOOTSTRAP_SERVERS", System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+        if (System.getenv().containsKey("GOST_SERVER"))
+            globalOptions.setProperty("GOST_SERVER", System.getenv("GOST_SERVER"));
         if (System.getenv().containsKey("FILTER_LOGIC"))
-            globalOptions.setProperty("filter-logic", System.getenv("FILTER_LOGIC"));
+            globalOptions.setProperty("FILTER_LOGIC", System.getenv("FILTER_LOGIC"));
 
         // parse input parameter to options and check completeness, must be a key-val pair
         if (1 == args.length % 2) {
@@ -44,7 +56,8 @@ public class StreamAppEngine {
             globalOptions.setProperty(key, args[i+1]);
         }
 
-        String[] keys = {"stream-name", "source-system", "target-system", "bootstrap-server", "filter-logic"};
+        String[] keys = {"STREAM_NAME", "SOURCE_SYSTEM", "TARGET_SYSTEM", "KAFKA_BOOTSTRAP_SERVERS",
+                "GOST_SERVER", "FILTER_LOGIC"};
         for (String key: keys) {
             if (!globalOptions.stringPropertyNames().contains(key)) {
                 logger.error("Error: You have to define the parameter " + key +
@@ -55,18 +68,25 @@ public class StreamAppEngine {
             logger.info("  " + key + ": " + globalOptions.getProperty(key));
         }
 
+
+        /**************************        load json from SensorThings         **************************/
+
+        reloadGOSTServer();
+        String name = sensorThingsStreams.get(Integer.toString(58)).getAsJsonObject().get("name").getAsString();
+        System.out.println(name);
+
 //        System.exit(99);
 
         /**************************   set up the topology and then start it    **************************/
         // create input and output topics from system name
-        String inputTopicName = globalOptions.getProperty("source-system") + ".int";
-        String targetTopic = globalOptions.getProperty("target-system") + ".ext";
+        String inputTopicName = globalOptions.getProperty("SOURCE_SYSTEM") + ".int";
+        String targetTopic = globalOptions.getProperty("TARGET_SYSTEM") + ".ext";
 
         // create properties
         Properties properties = new Properties();
-        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, globalOptions.getProperty("bootstrap-server"));
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, globalOptions.getProperty("KAFKA_BOOTSTRAP_SERVERS"));
         properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streamhub-" +
-                globalOptions.getProperty("source-system") + "." + globalOptions.getProperty("stream-name"));
+                globalOptions.getProperty("SOURCE_SYSTEM") + "." + globalOptions.getProperty("STREAM_NAME"));
         properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
         properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 
@@ -77,7 +97,7 @@ public class StreamAppEngine {
         KStream<String, String> inputTopic = streamsBuilder.stream(inputTopicName);
 
         // TODO apply filter logic
-        KStream<String, String> filteredStream = inputTopic.filter((k, value) -> check_where_condition(value));
+        KStream<String, String> filteredStream = inputTopic.filter((k, value) -> check_condition(value));
 
         filteredStream.to(targetTopic);
 
@@ -92,28 +112,88 @@ public class StreamAppEngine {
 
     public static Properties globalOptions = new Properties();
 
+    public static JsonObject sensorThingsStreams = new JsonObject();
+
     public static JsonParser jsonParser = new JsonParser();
 
-    public static boolean check_where_condition(String inputJson) {
+    public static boolean check_condition(String inputJson) {
+        return check_condition(inputJson, false);
+    }
+
+    public static boolean check_condition(String inputJson, boolean second_try) {
         // json library
+        String iot_id = "-1";
         try {
-            System.out.println(inputJson);
-            String logic = globalOptions.getProperty("filter_logic");
-            int iot_id;
+            System.out.println("Getting new kafka message: " + inputJson);
+            String logic = globalOptions.getProperty("FILTER_LOGIC");  // TODO apply logic
             double result;
             iot_id = jsonParser.parse(inputJson)
                     .getAsJsonObject()
                     .get("Datastream").getAsJsonObject()
-                    .get("@iot.id").getAsInt();
+                    .get("@iot.id").getAsString();
+
+            String quantity_name = sensorThingsStreams.get(iot_id).getAsJsonObject().get("name").getAsString();
+            System.out.println("  Quantity is: " + quantity_name);
+
             result = jsonParser.parse(inputJson)
                     .getAsJsonObject()
                     .get("result").getAsDouble();
-//            return quantity.endsWith(".Station_1.Air Temperature") && result < 10;
-            System.out.println(result < 10);
-            return result < 10;
+
+            return quantity_name.endsWith(".Station_1.Air Temperature") && result < 10;  // filter on name and condition
+
         } catch (NullPointerException e) {
+            if (!second_try) {
+                System.out.println("iot_id '" + iot_id + "' was not found, refetching sensorthings.");
+                reloadGOSTServer();
+                return check_condition(inputJson, second_try=true);
+            }
+            else {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+
+    /* reload GOST Server entries into json, forward with -1 if no iot_id is specified */
+    public static void reloadGOSTServer() {
+        reloadGOSTServer(-1);
+    }
+    public static void reloadGOSTServer(int iot_id) {
+        if (iot_id == -1) {
+            String urlString = "http://" + globalOptions.getProperty("GOST_SERVER").replace("\"", "");
+            urlString += "/v1.0/Datastreams";
+//            System.out.println(urlString);
+            StringBuilder result = new StringBuilder();
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            JsonElement rawJsonObject = jsonParser.parse(result.toString());
+            JsonArray rawJsonArray = rawJsonObject.getAsJsonObject().get("value").getAsJsonArray();
+
+            // the json value is not ordered properly, restructure such that we have {iot_id0: {}, iot_id1: {}, ...}
+            sensorThingsStreams = new JsonObject();  // set ST to jsonObject
+
+            for (int i=1; i < rawJsonArray.size(); i++ ) {
+                System.out.println("Adding " + rawJsonArray.get(i).getAsJsonObject().get("name").getAsString());
+                sensorThingsStreams.add(
+                        rawJsonArray.get(i).getAsJsonObject().get("@iot.id").getAsString(),
+                        rawJsonArray.get(i).getAsJsonObject());
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
-            return false;
+        }
+        } else {
+            System.out.println("Iot id was specified, however, not implemented yet.");
+            System.exit(5);
         }
     }
 }
