146863c399bcd2abafd64cd8acbfc5c055b6cd6c
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2020-03-17 10:05:53 +0100

    basic filter validation

--- server/views/StreamHandler/stream_checks.py
+++ server/views/StreamHandler/stream_checks.py
@@ -6,5 +6,15 @@ def is_valid(payload):
     :param payload: String instance from the database
     :return: True if valid, else False
     """
-    # TODO implement checks
+    logic = payload.get("FILTER_LOGIC", "")
+
+    # An empty filter_logic is valid
+    if logic == "":
+        return True
+
+    if logic.count("SELECT") != 1 or logic.count("FROM") != 1 or \
+            logic.count("WHERE") > 1:
+        return False
+
+    # TODO implement more checks in Java and get feedback if the node can be built
     return True
